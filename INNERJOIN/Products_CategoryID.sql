2c59c3d6ad831dca2587b7e414f6670344606ec0
kimbyeonggeun <qudrms2157@gmail.com>
2022-10-20 12:42:11 +0900

    update Question

--- INNERJOIN/Products_CategoryID.sql
+++ INNERJOIN/Products_CategoryID.sql
@@ -1,3 +1,5 @@
+-- Products에 CategoryID가 5,6을 표시, 내용 프로덕트 이름, 공급자 이름 출력
+
 SELECT Products.ProductName, Suppliers.SupplierName
 FROM Products INNER JOIN Suppliers
 ON Products.SupplierID = Suppliers.SupplierID
